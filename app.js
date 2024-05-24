9b98c3abc24c25ade89e833a887a9fb7812e5660
Sameerbaacha <sameeris6512@gmail.com>
2024-05-24 20:35:58 +0500

    update calculator

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-function getNumber(num){
-    var result = document.getElementById("result");
+function getNumber(num) {
+    var result = document.getElementById("inputValue");
     result.value += num;
 }
 
-function clearResult(){
-    var result = document.getElementById("result");
+function clearResult() {
+    var result = document.getElementById("inputValue");
     result.value = "";
 }
 
-function getResult(){
-    var result = document.getElementById("result");
+function deleteCharacter() {
+    var result = document.getElementById("inputValue");
+    result.value = result.value.slice(0, -1);
+}
+
+function getResult() {
+    var result = document.getElementById("inputValue");
     result.value = eval(result.value);
+
 }
